e25be795b5f537f7133838c780f629c2794fdebd
31089062 <31089062@089786-P33S09.macklabs.br>
2013-04-23 21:35:01 -0300

    Atualização para exercício 3

--- src/br/calebe/exemplos/ex01/Pedido.java
+++ src/br/calebe/exemplos/ex01/Pedido.java
@@ -3,6 +3,7 @@
  * @author 
  * Nomes: Carlos Damian             TIA: 310.4726-2
  *        Lucas Flóro e Silva       TIA: 310.8906-2
+ *        Rodrigo Drumond           TIA: 306.5574-9
  */
 
 package br.calebe.exemplos.ex01;
